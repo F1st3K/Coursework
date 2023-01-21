b92fc499315d1aa6b65d245d886ccc2b8c5a7410
F1st3K <kostinnikita988@gmail.com>
2023-01-21 10:25:31 +0300

    develop on colledge

---
+++ AISShopComputerParts/AISShopComputerParts/AutorizationForm.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace AISShopComputerParts
+{
+    partial class AutorizationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.login = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textbox = new System.Windows.Forms.TextBox();
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.captcha = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize) (this.picture)).BeginInit();
+            this.captcha.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(54, 71);
+            this.login.Margin = new System.Windows.Forms.Padding(5);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(226, 36);
+            this.login.TabIndex = 0;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(54, 135);
+            this.password.Margin = new System.Windows.Forms.Padding(5);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(226, 36);
+            this.password.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 107);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Пароль";
+            // 
+            // textbox
+            // 
+            this.textbox.Location = new System.Drawing.Point(3, 130);
+            this.textbox.Margin = new System.Windows.Forms.Padding(5);
+            this.textbox.Name = "textbox";
+            this.textbox.Size = new System.Drawing.Size(223, 36);
+            this.textbox.TabIndex = 5;
+            // 
+            // picture
+            // 
+            this.picture.Location = new System.Drawing.Point(8, 32);
+            this.picture.Margin = new System.Windows.Forms.Padding(5);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(210, 88);
+            this.picture.TabIndex = 6;
+            this.picture.TabStop = false;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(54, 403);
+            this.buttonExit.Margin = new System.Windows.Forms.Padding(5);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(226, 44);
+            this.buttonExit.TabIndex = 7;
+            this.buttonExit.Text = "Выйти из программы";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(54, 352);
+            this.buttonLogin.Margin = new System.Windows.Forms.Padding(5);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(226, 41);
+            this.buttonLogin.TabIndex = 8;
+            this.buttonLogin.Text = "Войти";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label4.Location = new System.Drawing.Point(85, 9);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(211, 41);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Авторизация";
+            // 
+            // captcha
+            // 
+            this.captcha.Controls.Add(this.picture);
+            this.captcha.Controls.Add(this.textbox);
+            this.captcha.Location = new System.Drawing.Point(54, 174);
+            this.captcha.Name = "captcha";
+            this.captcha.Size = new System.Drawing.Size(226, 165);
+            this.captcha.TabIndex = 10;
+            this.captcha.TabStop = false;
+            this.captcha.Text = "Каптча";
+            // 
+            // AutorizationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 461);
+            this.ControlBox = false;
+            this.Controls.Add(this.captcha);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.login);
+            this.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AutorizationForm";
+            this.Text = "Авторизация";
+            ((System.ComponentModel.ISupportInitialize) (this.picture)).EndInit();
+            this.captcha.ResumeLayout(false);
+            this.captcha.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox login;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textbox;
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox captcha;
+    }
+}
+
---
+++ Dump.sql
@@ -0,0 +1,164 @@
+CREATE DATABASE  IF NOT EXISTS `dbshopcomputerparts` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `dbshopcomputerparts`;
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: dbshopcomputerparts
+-- ------------------------------------------------------
+-- Server version	5.6.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Staffs`
+--
+
+DROP TABLE IF EXISTS `Staffs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Staffs` (
+  `idStaff` int(11) NOT NULL AUTO_INCREMENT,
+  `fullName` varchar(45) NOT NULL,
+  `phoneNumber` varchar(11) NOT NULL,
+  PRIMARY KEY (`idStaff`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Staffs`
+--
+
+LOCK TABLES `Staffs` WRITE;
+/*!40000 ALTER TABLE `Staffs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Staffs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories` (
+  `idCategory` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`idCategory`),
+  UNIQUE KEY `idCategory_UNIQUE` (`idCategory`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `idOrder` int(11) NOT NULL AUTO_INCREMENT,
+  `idsProducts` varchar(45) NOT NULL,
+  `countProducts` int(11) NOT NULL,
+  `staff` int(11) NOT NULL,
+  PRIMARY KEY (`idOrder`),
+  UNIQUE KEY `idOrder_UNIQUE` (`idOrder`),
+  KEY `idStaff_idx` (`staff`),
+  CONSTRAINT `staff` FOREIGN KEY (`staff`) REFERENCES `Staffs` (`idStaff`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `idProduct` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `characteristic` varchar(1000) NOT NULL,
+  `idCategory` int(11) NOT NULL,
+  PRIMARY KEY (`idProduct`),
+  UNIQUE KEY `idProduct_UNIQUE` (`idProduct`),
+  UNIQUE KEY `idCategory_UNIQUE` (`idCategory`),
+  CONSTRAINT `idCategory` FOREIGN KEY (`idCategory`) REFERENCES `categories` (`idCategory`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `idUser` int(11) NOT NULL AUTO_INCREMENT,
+  `fullName` varchar(45) NOT NULL,
+  `login` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `status` enum('admin','user') NOT NULL,
+  `idStaff` int(11) NOT NULL,
+  PRIMARY KEY (`idUser`),
+  UNIQUE KEY `idUser_UNIQUE` (`idUser`),
+  UNIQUE KEY `login_UNIQUE` (`login`),
+  KEY `idStaff_idx` (`idStaff`),
+  CONSTRAINT `idStaff` FOREIGN KEY (`idStaff`) REFERENCES `Staffs` (`idStaff`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-01-20 10:54:19
