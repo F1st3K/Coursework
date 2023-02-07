df22651c48114d56a48addd30f2574e3235a09fb
F1st3K <kostinnikita988@gmail.com>
2023-02-07 19:40:50 +0300

    create select, insertinto, updateset

--- AISShopComputerParts/AISShopComputerParts/Logic/MySql/MySqlQueryGenerator.cs
+++ AISShopComputerParts/AISShopComputerParts/Logic/MySql/MySqlQueryGenerator.cs
@@ -2,15 +2,55 @@
 
 namespace AISShopComputerParts.Logic.MySql
 {
-    internal sealed class MySqlQueryGenerator
+    internal static class MySqlQueryGenerator
     {
-        private static MySqlQueryGenerator _instance;
+        public static string Select(string column, string tableName, string condition)
+            => "SELECT " + column + " FROM " + tableName + " WHERE " + condition + ";";
 
-        public static MySqlQueryGenerator GetInstance()
+        public static string InsertInto(string tableName, string[,] massValues)
         {
-            if (_instance == null)
-                _instance = new MySqlQueryGenerator();
-            return _instance;
+            string values = "";
+            for (int i = 0; i < massValues.GetLength(0); i++)
+            {
+                values += "(";
+                for (int j = 0; j < massValues.GetLength(1); j++)
+                {
+                    values += massValues[j, i];
+                    if (j + 1 < massValues.GetLength(1))
+                        values += ", ";
+                }
+                values += ")";
+                if (i + 1 < massValues.GetLength(0))
+                    values += ", ";
+            }
+            return "INSERT INTO " + tableName + " VALUES " + values + ";";
+        }
+
+        public static string InsertInto(string tableName, params string[] massValue)
+        {
+
+            string values = "";
+            values += "(";
+            for (int i = 0; i < massValue.Length; i++)
+            {
+                values += massValue[i];
+                if (i + 1 < massValue.Length)
+                    values += ", ";
+            }
+            values += ")";
+            return "INSERT INTO " + tableName + " VALUES " + values + ";";
+        }
+
+        public static string UpdateSet(string tableName, string condition, params string[] massValue)
+        {
+            string values = "";
+            for (int i = 0; i < massValue.Length; i++)
+            {
+                values += massValue[i];
+                if (i + 1 < massValue.Length)
+                    values += ", ";
+            }
+            return "UPDATE " + tableName + " SET " + values + " WHERE " + condition + ";";
         }
 
     }
