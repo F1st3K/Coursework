2ffeff679c204228623faf47851101c1c4c4d762
F1st3K <kostinnikita988@gmail.com>
2023-02-22 08:20:10 +0300

    first autorisation

--- AISShopComputerParts/AISShopComputerParts/Forms/AutorizationForm.cs
+++ AISShopComputerParts/AISShopComputerParts/Forms/AutorizationForm.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
 
 namespace AISShopComputerParts
 {
     public partial class AutorizationForm : Form
     {
+        private int _countTryPasswd = 8;
+        private int _countTryOutCaptcher = 3;
+        private int _msFrezze = 5000;
+
         public AutorizationForm()
         {
             InitializeComponent();
@@ -12,10 +18,43 @@ namespace AISShopComputerParts
 
         private void buttonLogin_Click(object sender, EventArgs e)
         {
-            Form form;
-            if (login.Text == "admin")
-                form = new AdminPanel();
-            else form = new ManagerPanel();
+            if (_countTryOutCaptcher <= 0)
+                ViewCaptcher();
+            if (_countTryPasswd <= 0)
+                Frezze();
+            _countTryOutCaptcher--;
+            _countTryPasswd--;
+        }
+
+        private void ViewCaptcher()
+        {
+            
+        }
+
+        private void Frezze()
+        {
+            var timer = new Timer();
+            timer.Interval = _msFrezze;
+            timer.Tick += (a, e) => {
+                
+            };
+        }
+
+        private void LogIn()
+        {
+            
+        }
+
+        private void LoginUser()
+        {
+            var form = new ManagerPanel();
+            this.Hide();
+            form.Show();
+        }
+
+        private void LoginAdmin()
+        {
+            var form = new AdminPanel();
             this.Hide();
             form.Show();
         }
